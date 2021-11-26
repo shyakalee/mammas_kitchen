b985af81d503da3ca7dfdaf9290c9abb19d11e78
SHYAKA Leonce <shyakalee@gmail.com>
2021-11-26 06:50:58 +0200

    first commit of files

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <meta name="viewport" content="width=device-width, initial=1.0">
+    <style src="styles.css" type="text/css" rel="stylesheet"></style>
+    <head>
+        <title>This is my title of website</title>
+    </head>
+    <body class="main">
+        <span>
+            <p>Span title</p>
+            <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Rem quis cumque quidem modi minima nemo quas consequuntur error earum excepturi sit tempore quam pariatur nam, 
+                culpa quisquam at voluptatum sunt. 
+            </small>
+
+        </span>
+    </body>
+</html>
